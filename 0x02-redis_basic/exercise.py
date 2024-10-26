598ade97197e59394bebd5747299746231cef550
Ahmed-Almakki <ahmedlmakkii@gmail.com>
2024-10-26 01:00:42 +0000

    use fn function

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """ writting strings to redis"""
 import uuid
-from typing import Union
+from typing import Union, Callable
 import redis
+import struct
 
 
 class Cache:
@@ -17,3 +18,21 @@ class Cache:
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(key: str, fn: Callable = None) -> Union[None, str, bytes, int, float]:
+        """ return the value of the key"""
+        if self._redis.exists(key):
+            data = self._redis.get(key)
+            if fn:
+                return fn(data)
+            return data
+        return None
+
+    def get_str(self, key: str) -> Union[None, str]:
+        """ return a string"""
+        return self.get(key, lambda x: x.decode("utf-8") if x else None)
+
+    def get_int(self, key: str) -> Union[None, int]:
+        """return a int"""
+        return self.get(key, lambda x: int.from_bytes(x, "big") if x else None)
+
